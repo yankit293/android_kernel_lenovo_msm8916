32d44a7aeb69643211ae6e7444b9c27cd4885e36
Ritesh Harjani <riteshh@codeaurora.org>
2015-03-20 14:50:47 +0530

    mmc: core: Fix mmc clk scale down during writes.
    No need to check for valid state when we want to
    scale down the clocks. As this state validity
    check is to only address that clk scaling
    doesnt scale up (which will send tuning
    cmds) during definite cmds sequences (like
    erase operation).
    
    Change-Id: Iee970b68698605c5335bbaba58c6dcaaa112c82b
    CRs-Fixed: 800478
    Signed-off-by: Ritesh Harjani <riteshh@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -3032,10 +3032,9 @@ static bool mmc_is_vaild_state_for_clk_scaling(struct mmc_host *host,
 	 * this mode.
 	 */
 	if (!card || (mmc_card_mmc(card) &&
-		card->part_curr == EXT_CSD_PART_CONFIG_ACC_RPMB) ||
-		(host->clk_scaling.invalid_state &&
-		!(state == MMC_LOAD_LOW &&
-		host->clk_scaling.scale_down_in_low_wr_load)))
+			card->part_curr == EXT_CSD_PART_CONFIG_ACC_RPMB)
+			|| (state != MMC_LOAD_LOW &&
+				host->clk_scaling.invalid_state))
 		goto out;
 
 	if (mmc_send_status(card, &status)) {
