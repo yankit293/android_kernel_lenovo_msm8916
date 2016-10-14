479b30ad58abdedb7f62f6ea20a66f369a45d613
Benjamin Legrand <android@legrand.ws>
2016-10-14 17:39:30 +0200

    Revert "mfd: wcd9xxx-core: write same register with multiple values"
    This reverts commit cfc12c1e2b4b7e3979f815ce764d8517050e2a0b.

--- drivers/mfd/wcd9xxx-core.c
+++ drivers/mfd/wcd9xxx-core.c
@@ -758,7 +758,7 @@ static int __wcd9xxx_slim_write_repeat(struct wcd9xxx *wcd9xxx,
 }
 
 /*
- * wcd9xxx_bus_write_repeat: Write the same register with multiple values
+ * wcd9xxx_slim_write_repeat: Write the same register with multiple values
  * @wcd9xxx: handle to wcd core
  * @reg: register to be written
  * @bytes: number of bytes to be written to reg
@@ -766,7 +766,7 @@ static int __wcd9xxx_slim_write_repeat(struct wcd9xxx *wcd9xxx,
  * This API will write reg with bytes from src in a single slimbus
  * transaction. All values from 1 to 16 are supported by this API.
  */
-int wcd9xxx_bus_write_repeat(struct wcd9xxx *wcd9xxx, unsigned short reg,
+int wcd9xxx_slim_write_repeat(struct wcd9xxx *wcd9xxx, unsigned short reg,
 			      int bytes, void *src)
 {
 	int ret = 0;
@@ -777,21 +777,11 @@ int wcd9xxx_bus_write_repeat(struct wcd9xxx *wcd9xxx, unsigned short reg,
 		if (ret)
 			goto err;
 
-		if (wcd9xxx_get_intf_type() == WCD9XXX_INTERFACE_TYPE_I2C) {
-			ret = wcd9xxx->write_dev(wcd9xxx, reg, bytes, src,
-								false);
-			if (ret < 0)
-				dev_err(wcd9xxx->dev,
-					"%s: Codec repeat write failed (%d)\n",
-					__func__, ret);
-		} else {
-			ret = __wcd9xxx_slim_write_repeat(wcd9xxx, reg, bytes,
-								src);
-			if (ret < 0)
-				dev_err(wcd9xxx->dev,
-					"%s: Codec repeat write failed (%d)\n",
-					__func__, ret);
-		}
+		ret = __wcd9xxx_slim_write_repeat(wcd9xxx, reg, bytes, src);
+		if (ret < 0)
+			dev_err(wcd9xxx->dev,
+				"%s: Codec repeat write failed (%d)\n",
+				__func__, ret);
 	} else {
 		ret = __wcd9xxx_slim_write_repeat(wcd9xxx, reg, bytes, src);
 	}
@@ -799,7 +789,7 @@ err:
 	mutex_unlock(&wcd9xxx->io_lock);
 	return ret;
 }
-EXPORT_SYMBOL(wcd9xxx_bus_write_repeat);
+EXPORT_SYMBOL(wcd9xxx_slim_write_repeat);
 
 /*
  * wcd9xxx_slim_reserve_bw: API to reserve the slimbus bandwidth
