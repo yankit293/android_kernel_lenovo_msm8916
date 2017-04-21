300a3e90682bc4aceae1f6ba65cbdf00a3af5a83
DreamStar001 <nikitsharma92@gmail.com>
2017-04-21 11:58:33 +0530

    Squash update

--- drivers/input/touchscreen/ft5x06_ts.c
+++ drivers/input/touchscreen/ft5x06_ts.c
@@ -32,6 +32,11 @@
 #include <linux/sensors.h>
 #include <linux/input/ft5x06_ts.h>
 
+#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
+#include <linux/input/prevent_sleep.h>
+bool dit_suspend = false;
+#endif
+
 #if defined(CONFIG_FB)
 #include <linux/notifier.h>
 #include <linux/fb.h>
@@ -1093,7 +1098,24 @@ static int ft5x06_ts_stop(struct device *dev)
 	char txbuf[2];
 	int i, err;
 
+#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
+  	ts_get_prevent_sleep(prevent_sleep);
+  	DT2W_PRINFO("%s: Prevent Sleep is computed as '%s'\n",
+  			__func__, (prevent_sleep) ? "yes" : "no");
+  	if (!prevent_sleep) {
+  		DT2W_PRINFO("%s: IRQ now disable_irq()\n", __func__);
+  		disable_irq(data->client->irq);
+  		dit_suspend = false;
+  	} else {
+  		DT2W_PRINFO("%s: IRQ now enable_irq_wake()\n", __func__);
+ 		enable_irq_wake(data->client->irq);
+ 		dit_suspend = true;
+ 	}
+#else
+
 	disable_irq(data->client->irq);
+
+#endif
 
 	/* release all touches */
 	for (i = 0; i < data->pdata->num_max_touches; i++) {
@@ -1105,7 +1127,12 @@ static int ft5x06_ts_stop(struct device *dev)
 
 	if (gpio_is_valid(data->pdata->reset_gpio)) {
 		txbuf[0] = FT_REG_PMODE;
+#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
+ 		txbuf[1] = (!prevent_sleep) ? FT_PMODE_HIBERNATE :
+ 			FT_PMODE_MONITOR;
+#else
 		txbuf[1] = FT_PMODE_HIBERNATE;
+#endif
 		ft5x06_i2c_write(data->client, txbuf, sizeof(txbuf));
 	}
 
@@ -1172,6 +1199,10 @@ static int ft5x06_ts_suspend(struct device *dev)
 	struct ft5x06_ts_data *data = dev_get_drvdata(dev);
 	int err;
 
+#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
+	bool prevent_sleep = false;
+#endif
+
 	if (data->loading_fw) {
 		dev_info(dev, "Firmware loading in process...\n");
 		return 0;
@@ -1204,12 +1235,54 @@ static int ft5x06_ts_suspend(struct device *dev)
 			dev_err(&data->client->dev,
 				"%s: set_irq_wake failed\n", __func__);
 		data->suspended = true;
+#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
+  	if (prevent_sleep) {
+  		/* disable the key panel touches */
+  		__clear_bit(EV_KEY, data->input_dev->evbit);
+  		input_sync(data->input_dev);
+  	}
+  	data->prevent_sleep = prevent_sleep;
+#endif
+
 		return err;
 	}
 
 	return ft5x06_ts_stop(dev);
 }
 
+#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
+static int dt2w_toggle_rebalance_irq(struct device *dev)
+{
+  	struct ft5x06_ts_data *data = dev_get_drvdata(dev);
+  
+  	/* 
+  	 * Prema Chand Alugu (premaca@gmail.com)
+  	 *
+  	 * This function must be called only when we know that prevent_sleep state
+  	 * has been changed while the screen off.
+  	 */
+  	if (data->prevent_sleep) {
+  		// we have been with wake irqs so far [enable_irq_wake()]
+  		disable_irq_wake(data->client->irq);
+  		disable_irq(data->client->irq);
+  		DT2W_PRINFO("%s: IRQ now disable_irq_wake()\n", __func__);
+  		DT2W_PRINFO("%s: IRQ now disable_irq()\n", __func__);
+  		DT2W_PRINFO("%s: Rebalanced IRQ while dt2w OFF "
+  				"during screen-off\n", __func__);
+  	} else {
+  		// we have been with irqs so far [disable_irq()]
+  		enable_irq(data->client->irq);
+  		enable_irq_wake(data->client->irq);
+  		DT2W_PRINFO("%s: IRQ now enable_irq()\n", __func__);
+  		DT2W_PRINFO("%s: IRQ now enable_irq_wake()\n", __func__);
+  		DT2W_PRINFO("%s: Rebalanced IRQ while dt2w ON "
+  				"during screen-off\n", __func__);
+  	}
+  
+ 	return 0;
+}
+#endif
+ 
 static int ft5x06_ts_resume(struct device *dev)
 {
 	struct ft5x06_ts_data *data = dev_get_drvdata(dev);
@@ -2278,11 +2351,17 @@ static int ft5x06_ts_probe(struct i2c_client *client,
 
 	err = request_threaded_irq(client->irq, NULL,
 				ft5x06_ts_interrupt,
+#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
+ 							   pdata->irq_gpio_flags | IRQF_ONESHOT
+ 							   | IRQF_NO_SUSPEND,
+#else
+
 	/*
 	* the interrupt trigger mode will be set in Device Tree with property
 	* "interrupts", so here we just need to set the flag IRQF_ONESHOT
 	*/
 				IRQF_ONESHOT,
+#endif
 				client->dev.driver->name, data);
 	if (err) {
 		dev_err(&client->dev, "request irq failed\n");
--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -24,6 +24,7 @@
 #include <linux/leds-qpnp-wled.h>
 #include <linux/clk.h>
 #include <linux/pm_qos.h>
+#include <linux/lcd_notify.h>
 
 #include "mdss.h"
 #include "mdss_panel.h"
@@ -1423,6 +1424,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		if (ctrl_pdata->refresh_clk_rate)
 			rc = mdss_dsi_clk_refresh(pdata);
 		mdss_dsi_get_hw_revision(ctrl_pdata);
+		lcd_notifier_call_chain(LCD_EVENT_ON_START, NULL);
 		if (ctrl_pdata->on_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_unblank(pdata);
 		break;
@@ -1434,8 +1436,10 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		if (ctrl_pdata->on_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_unblank(pdata);
 		pdata->panel_info.esd_rdy = true;
+		lcd_notifier_call_chain(LCD_EVENT_ON_END, NULL);
 		break;
 	case MDSS_EVENT_BLANK:
+		lcd_notifier_call_chain(LCD_EVENT_OFF_START, NULL);
 		power_state = (int) (unsigned long) arg;
 		if (ctrl_pdata->off_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_blank(pdata, power_state);
@@ -1447,6 +1451,8 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 			rc = mdss_dsi_blank(pdata, power_state);
 		if (!(pdata->panel_info.mipi.always_on))
 			rc = mdss_dsi_off(pdata, power_state);
+		rc = mdss_dsi_off(pdata, power_state);
+		lcd_notifier_call_chain(LCD_EVENT_OFF_END, NULL);
 		break;
 	case MDSS_EVENT_CONT_SPLASH_FINISH:
 		if (ctrl_pdata->off_cmds.link_state == DSI_LP_MODE)
@@ -2319,3 +2325,4 @@ module_exit(mdss_dsi_driver_cleanup);
 MODULE_LICENSE("GPL v2");
 MODULE_DESCRIPTION("DSI controller driver");
 MODULE_AUTHOR("Chandan Uddaraju <chandanu@codeaurora.org>");
+
