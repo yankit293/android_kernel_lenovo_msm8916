8cfe858cd848ebc43572a1b067bd4ef0015d259d
DreamStar001 <nikitsharma92@gmail.com>
2017-05-14 22:10:32 +0530

    Revert "sched: Limit LBF_PWR_ACTIVE_BALANCE to within cluster"
    This reverts commit 3537fa5c8f12dfd3357ffeb7a33d5bb49c958dc2.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6190,7 +6190,7 @@ static bool update_sd_pick_busiest(struct lb_env *env,
 	/* Mark a less power-efficient CPU as busy only if we haven't
 	 * seen a busy group yet. We want to prioritize spreading
 	 * work over power optimization. */
-	if (!sds->busiest && (capacity(env->dst_rq) == group_rq_capacity(sg)) &&
+	if (!sds->busiest && sg->group_weight == 1 &&
 	    sgs->sum_nr_running && (env->idle != CPU_NOT_IDLE) &&
 	    power_cost_at_freq(env->dst_cpu, 0) <
 	    power_cost_at_freq(cpumask_first(sched_group_cpus(sg)), 0)) {
